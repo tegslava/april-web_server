20efb59d6e2c3611853669d5d5ef30c7536cd0f6
Vyacheslav Tegneryadnov <tegs@mail.ru>
2024-04-29 21:59:13 +0300

    Д/з 24 убрал обработку HttpRequest в поток

--- src/main/java/ru/otus/tegneryadnov/web/server/HttpServer.java
+++ src/main/java/ru/otus/tegneryadnov/web/server/HttpServer.java
@@ -16,47 +16,42 @@ public class HttpServer {
     }
 
     public void start() {
+        ExecutorService service = null;
         try (ServerSocket serverSocket = new ServerSocket(port)) {
-            ExecutorService service = Executors.newFixedThreadPool(PROCESSED_REQUESTS);
+            service = Executors.newFixedThreadPool(PROCESSED_REQUESTS);
             System.out.println("Сервер запущен на порту: " + port);
             this.dispatcher = new Dispatcher();
             System.out.println("Диспетчер проинициализирован");
             while (!serverSocket.isClosed()) {
                 Socket socket = serverSocket.accept();
-                byte[] buffer = new byte[8192];
-                int n = socket.getInputStream().read(buffer);
-                if (n > -1) {
-                    String rawRequest = new String(buffer, 0, n);
-                    HttpRequest request = new HttpRequest(rawRequest);
-                    request.info(true);
-                    if (request.getUri().equalsIgnoreCase("/shutdown")) {
-                        System.out.println("Поступила команда SHUTDOWN");
-                        if (!socket.isClosed()) {
-                            socket.close();
-                        }
-                        break;
-                    }
-                    service.execute(() ->
-                    {
-                        try {
+                service.execute(() ->
+                {
+                    try {
+                        byte[] buffer = new byte[8192];
+                        int n = socket.getInputStream().read(buffer);
+                        if (n > -1) {
+                            String rawRequest = new String(buffer, 0, n);
+                            HttpRequest request = new HttpRequest(rawRequest);
+                            request.info(true);
                             dispatcher.execute(request, socket.getOutputStream());
+                        }
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    } finally {
+                        try {
+                            socket.close();
                         } catch (IOException e) {
                             throw new RuntimeException(e);
-                        } finally {
-                            try {
-                                socket.close();
-                            } catch (IOException e) {
-                                throw new RuntimeException(e);
-                            }
                         }
-                    });
-                } else if (!socket.isClosed()) {
-                    socket.close();
-                }
+                    }
+                });
             }
-            service.shutdown();
         } catch (IOException e) {
             e.printStackTrace();
+        } finally {
+            if (service != null) {
+                service.shutdown();
+            }
         }
     }
 }
