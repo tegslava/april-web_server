fcc4b10de44cbd8e637bc92928a1535295fd478d
Vyacheslav Tegneryadnov <tegs@mail.ru>
2024-04-29 22:51:49 +0300

    Д/з 24 добавил обработку shutdown

--- src/main/java/ru/otus/tegneryadnov/web/server/HttpServer.java
+++ src/main/java/ru/otus/tegneryadnov/web/server/HttpServer.java
@@ -10,6 +10,7 @@ public class HttpServer {
     private final static int PROCESSED_REQUESTS = 4;
     private final int port;
     private Dispatcher dispatcher;
+    private static boolean working;
 
     public HttpServer(int port) {
         this.port = port;
@@ -17,12 +18,14 @@ public class HttpServer {
 
     public void start() {
         ExecutorService service = null;
+        working = true;
         try (ServerSocket serverSocket = new ServerSocket(port)) {
             service = Executors.newFixedThreadPool(PROCESSED_REQUESTS);
             System.out.println("Сервер запущен на порту: " + port);
             this.dispatcher = new Dispatcher();
             System.out.println("Диспетчер проинициализирован");
-            while (!serverSocket.isClosed()) {
+            // while (!serverSocket.isClosed()) {
+            while (working) {
                 Socket socket = serverSocket.accept();
                 service.execute(() ->
                 {
@@ -33,6 +36,13 @@ public class HttpServer {
                             String rawRequest = new String(buffer, 0, n);
                             HttpRequest request = new HttpRequest(rawRequest);
                             request.info(true);
+                            if (request.getUri().equalsIgnoreCase("/shutdown")) {
+                                System.out.println("Поступила команда SHUTDOWN\nСервер выключается");
+                                synchronized (HttpServer.class) {
+                                    working = false;
+                                }
+                                return;
+                            }
                             dispatcher.execute(request, socket.getOutputStream());
                         }
                     } catch (IOException e) {
